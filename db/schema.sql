da31a160ff7ddb223a54a5618d93f267bce351df
Hongjing <hongjingdeng@gmail.com>
2020-08-31 01:50:22 -0400

    adding more images

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
+DROP DATABASE burgers_db;
 CREATE DATABASE burgers_db;
+
 USE burgers_db;
 
 -- create table burgers -- 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY(id),
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(80) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY(id)
 );
