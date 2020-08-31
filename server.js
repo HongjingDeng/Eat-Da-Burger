031f58254e4572444744b1f6e4a8b027c4424ff8
Hongjing <hongjingdeng@gmail.com>
2020-08-31 02:40:30 -0400

    correcting server errors and main handlebars

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-//dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
-var mysql = require("mysql");
 
-//set up the Exprepress App
-//create an instance of the express app
 var app = express();
+var PORT = process.env.PORT || 3000;
 
-//set the port of this app
-//process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
 
-//set Handlebars as the default templating engine
-app.engine("handlebars", exphbs({defaultLayout: "main" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
